1160ac8f5bc1fd30c7421b78d199e7ae0836fab4
Imran Shoukat <imran.shoukat1995@gmail.com>
2016-12-10 18:38:59 +0500

    Display the assigned agent name in Summary of chat sessions and Canned Response's Screen doesn't update in real time

--- shared/container/CannedResponse/CannedResponses.js
+++ shared/container/CannedResponse/CannedResponses.js
@@ -16,7 +16,7 @@ import { browserHistory } from 'react-router'
 class CannedResponses extends Component {
 
  constructor(props, context) {
-      //call action to get user teams 
+      //call action to get user teams
     if(props.userdetails.accountVerified == "No"){
     browserHistory.push('/notverified');
    }
@@ -24,7 +24,7 @@ class CannedResponses extends Component {
     console.log('constructor is called');
     if(usertoken != null)
     {
-       
+
         console.log(usertoken);
         props.getresponses(usertoken)
       }
@@ -34,11 +34,11 @@ class CannedResponses extends Component {
     };
      this.handleClick = this.handleClick.bind(this);
      this.add = this.add.bind(this);
-  
 
-  
 
-    
+
+
+
   }
  handleClick(e) {
 
@@ -52,14 +52,14 @@ class CannedResponses extends Component {
      const usertoken = auth.getToken();
      var companyid = this.props.userdetails.uniqueid;
 
-      
+
      this.props.createResponse({shortcode,message,companyid,usertoken});
      this.setState({
       showCR: false,
     });
  }
 
- 
+
 
   render() {
     console.log(this.props.userdetails.firstname)
@@ -69,31 +69,31 @@ class CannedResponses extends Component {
     return (
       <div>
        <AuthorizedHeader name = {this.props.userdetails.firstname} user={this.props.userdetails}/>
-    
+
        <div className="page-container">
-         <SideBar isAdmin ={this.props.userdetails.isAdmin}/> 
+         <SideBar isAdmin ={this.props.userdetails.isAdmin}/>
           <div className="page-content-wrapper">
-            <div className="page-content"> 
+            <div className="page-content">
               <h3 className ="page-title">Canned Response Management </h3>
             <ul className="page-breadcrumb breadcrumb">
                   <li>
                     <i className="fa fa-home"/>
                     <Link to="/dashboard"> Dashboard </Link>
-                    <i className="fa fa-angle-right"/> 
-                  </li>                  
+                    <i className="fa fa-angle-right"/>
+                  </li>
                   <li>
                                <Link to="/cannedresponses">Canned Response Management</Link>
-                  </li>               
-  
+                  </li>
+
             </ul>
             <div className="portlet box grey-cascade">
               <div className="portlet-title">
                 <div className="caption">
                     <i className="fa fa-user"/>
                    Canned Response
-                </div> 
-              </div>    
-        
+                </div>
+              </div>
+
            <div className="portlet-body">
              <div className="table-toolbar">
                  <div className="btn-team">
@@ -102,9 +102,9 @@ class CannedResponses extends Component {
                      <button id="sample_editable_1_new" className="btn green" onClick={this.handleClick}> Add Canned Response
                     <i className="fa fa-plus"/>
                     </button>
-                
-                    
-                    
+
+
+
                  }
 
                  </div>
@@ -113,7 +113,7 @@ class CannedResponses extends Component {
 
                      <div className = "alert alert-danger"><span>{this.props.errorMessage}</span></div>
                       }
-              <CannedResponseCreate addResponse={this.add}  showCR= {this.state.showCR}/>      
+              <CannedResponseCreate addResponse={this.add}  showCR= {this.state.showCR}/>
                 { this.props.responses &&
                    <table id ="sample_3" className="table table-striped table-bordered table-hover dataTable">
                    <thead>
@@ -122,33 +122,33 @@ class CannedResponses extends Component {
                     <th role="columnheader" rowspan='1' colspan='1' aria-sort='ascending' >Response</th>
                       { this.props.userdetails.isAgent == "Yes"?<br/> :
                          <th role="columnheader" rowspan='1' colspan='1' aria-sort='ascending' > Options</th>
-           
+
 
                      }
-                   
+
                     </tr>
                     </thead>
 
-                    <tbody>                    
+                    <tbody>
                       {
                         this.props.responses &&
                         this.props.responses.map((response, i) => (
-                          
+
                           <ResponseListItem response={response} key={response._id}  onDelete={() => this.props.deleteresponse(response,token)} userdetails={this.props.userdetails}/>
-                                                      
+
                         ))
                       }
-                     </tbody> 
+                     </tbody>
                     </table>
                 }
-        
-                
+
+
             </div>
           </div>
        </div>
-       </div> 
+       </div>
       </div>
-      </div> 
+      </div>
   )
   }
 }
@@ -167,7 +167,7 @@ function mapStateToProps(state) {
           responses :(state.dashboard.responses),
           agents:(state.dashboard.agents),
           deptagents:(state.dashboard.deptagents),
-          
+
            };
 
 }
@@ -176,6 +176,3 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({getresponses:getresponses,deleteresponse:deleteresponse,createResponse:createResponse}, dispatch);
 }
 export default connect(mapStateToProps,mapDispatchToProps)(CannedResponses);
-
-
-
