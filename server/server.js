d3161a339252ed677c4c1b83720fc4f6269e14f5
Zarmeen <zarmeennasim@yahoo.com>
2017-03-08 16:10:41 +0500

    removing unwanted css files

--- server/server.js
+++ server/server.js
@@ -84,13 +84,7 @@ const renderFullPage = (html, initialState) => {
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
         <!--<link href="/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>-->
-        <link href="https://www.cloudkibo.com/admin_assets/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
-        <link href="https://www.cloudkibo.com/admin_assets/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="https://www.cloudkibo.com/admin_assets/assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
-        <link href="https://www.cloudkibo.com/admin_assets/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css"/>
-        <link href="https://www.cloudkibo.com/admin_assets/assets/global/css/components.css" id="style_components" rel="stylesheet" type="text/css"/>
-        <link href="https://www.cloudkibo.com/admin_assets/assets/global/css/plugins.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/admin/layout/css/themes/darkblue.css" rel="stylesheet" />
+         <link href="assets/admin/layout/css/themes/darkblue.css" rel="stylesheet" />
 
          <link rel="stylesheet" href='/css/bootstrap/css/bootstrap.min.css' />
          <link rel="stylesheet" href='/css/style.css' />
