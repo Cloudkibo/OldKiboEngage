b1c41cb0933bf6aa32e8a19105ebf22ad408258e
ImranShoukat <imran.shoukat1995@gmail.com>
2017-05-27 23:23:33 +0500

    #532 fixed

--- shared/container/SubGroup/SubGroupCreate.js
+++ shared/container/SubGroup/SubGroupCreate.js
@@ -11,18 +11,18 @@ import { Link } from 'react-router';
 
 class SubgroupCreate extends Component {
   constructor(props, context) {
-       //call action to get user teams 
+       //call action to get user teams
     const usertoken = auth.getToken();
     console.log('constructor is called');
     if(!props.customers){
           props.getcustomers(usertoken);
       }
     super(props, context);
-    
+
     this.createSubgroup = this.createSubgroup.bind(this);
   }
 
- 
+
 
   createSubgroup(e) {
      e.preventDefault();
@@ -31,6 +31,8 @@ class SubgroupCreate extends Component {
     const descRef = this.refs.desc;
     const groupid = this.refs.groupid;
 
+    if(this.props.subgroups.filter((c) => c.groupid == groupid.value).length < this.props.companysettings.maxnumberofchannels) {
+
     var companyid;
     var createdBy = this.props.userdetails._id;
 
@@ -38,51 +40,55 @@ class SubgroupCreate extends Component {
      {
       var subgroup = {'msg_channel_name' : nameRef.value,'msg_channel_description':descRef.value,'companyid' : this.props.userdetails.uniqueid,'groupid' : groupid.value,'createdby' : createdBy}
       this.props.createSubgroup(subgroup,usertoken,this.props.customers.filter((c) => c.isMobileClient == "true"));
-     
+
     }
+  }
+  else {
+    alert(`Maximum number of subgroups per group has set to ${this.props.companysettings.maxnumberofchannels}. You cannot create more. If you want to create more subgroups, please go to compnay settings and update the limit.`);
+  }
   }
   handleChange(e){
       const groupidRef =  this.refs.groupid;
       groupidRef.value =  e.target.value;
-   
+
     }
-    
-    
+
+
   render() {
     return (
       <div className="vbox viewport">
        <AuthorizedHeader name = {this.props.userdetails.firstname} user={this.props.userdetails}/>
- 
+
        <div className="page-container hbox space-between">
-         <SideBar isAdmin ={this.props.userdetails.isAdmin}/> 
+         <SideBar isAdmin ={this.props.userdetails.isAdmin}/>
           <div className="page-content-wrapper">
-            <div className="page-content"> 
+            <div className="page-content">
               <h3 className ="page-title">SubGroups Management </h3>
             <ul className="page-breadcrumb breadcrumb">
                   <li>
                     <i className="fa fa-home"/>
                     <Link to="/dashboard"> Dashboard </Link>
-                    <i className="fa fa-angle-right"/> 
-                  </li>                  
+                    <i className="fa fa-angle-right"/>
+                  </li>
                   <li>
                                <Link to="/subgroups"> SubGroups Management</Link>
-                  </li>               
-  
+                  </li>
+
             </ul>
                 {this.props.errorMessage &&
 
                      <div className = "alert alert-danger"><span>{this.props.errorMessage}</span></div>
                       }
-         
-            
+
+
             <div className="portlet box grey-cascade">
               <div className="portlet-title">
                 <div className="caption">
                     <i className="fa fa-group"/>
                    Create Subgroup
-                </div> 
-              </div>    
-        
+                </div>
+              </div>
+
            <div className="portlet-body form">
             <form className="form-horizontal form-row-seperated">
               <div className="form-body">
@@ -90,7 +96,7 @@ class SubgroupCreate extends Component {
                   <label className="control-label col-md-3"> Sub Group Name </label>
                    <div className="col-md-9">
                          <input className="form-control input-medium" type='text'  ref = "name"/>
-                
+
 
                    </div>
                 </div>
@@ -103,23 +109,23 @@ class SubgroupCreate extends Component {
                 </div>
                  <div className="form-group">
                   <label className="control-label col-md-3"> Select Group </label>
-                  <div className="col-md-9">   
+                  <div className="col-md-9">
                   {
                         <select  ref = "groupid" onChange={this.handleChange.bind(this)}   >
                           {
                            this.props.groupdetails &&
-                        
+
                            this.props.groupdetails.map((group, i) => (
-                          
+
                           <option value={group._id} key={group._id}> {group.deptname}</option>
-                                                      
+
                         ))
                         }
                         </select>
                     }
                       </div>
                 </div>
-            
+
               <div className="form-actions fluid">
               <div className="row">
                 <div className="col-md-3">
@@ -130,33 +136,33 @@ class SubgroupCreate extends Component {
                     </button>
 
                     </div>
-               </div> 
+               </div>
                 <div className="col-md-9">
                   <div className="col-md-9">
                     <Link to="/subgroups" className="btn green">
                       <i className="fa fa-times"/>
                        Back
                     </Link>
-                    
+
                     </div>
                </div>
-               </div>                 
+               </div>
               </div>
-              </div>  
-              
+              </div>
+
           </form>
 
-                  
-          
+
+
           </div>
           </div>
-        
+
 
        </div>
-       </div> 
+       </div>
       </div>
-      </div> 
-      )                   
+      </div>
+      )
      }
 }
 
@@ -164,7 +170,7 @@ class SubgroupCreate extends Component {
 function mapStateToProps(state) {
   console.log("mapStateToProps is called");
   console.log(state.dashboard.agent);
-  
+
    return {
     groupdetails:(state.dashboard.groupdetails),
     userdetails:(state.dashboard.userdetails),
@@ -173,8 +179,7 @@ function mapStateToProps(state) {
     deptagents:(state.dashboard.deptagents),
     subgroups :(state.dashboard.subgroups),
     customers:(state.dashboard.customers),
+    companysettings: (state.dashboard.companysettings),
 };
 }
 export default connect(mapStateToProps,{createSubgroup,getcustomers})(SubgroupCreate);
-
-
