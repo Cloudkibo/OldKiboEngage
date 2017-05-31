2219932a301002a1266e924b6be628f92be47f2e
Zarmeen <zarmeennasim@yahoo.com>
2017-05-31 11:10:48 +0500

    messages sort issue after resolving session resolved

--- shared/container/facebookChat/ChatArea.js
+++ shared/container/facebookChat/ChatArea.js
@@ -232,7 +232,7 @@ assignSessionToTeam(e){
               assignedagentemail: agentemail,
 
             }
-    this.props.add_socket_fb_message(data,this.props.fbchats,this.props.senderid,this.props.fbsessions);
+    this.props.add_socket_fb_message(data,this.props.fbchats,this.props.senderid,this.props.fbsessions,this.props.sessionsortorder);
 
      //3. update agent assignment table on server
 
@@ -359,7 +359,7 @@ autoassignChat(){
 
 
             }
-    this.props.add_socket_fb_message(data,this.props.fbchats,this.props.senderid,this.props.fbsessions)
+    this.props.add_socket_fb_message(data,this.props.fbchats,this.props.senderid,this.props.fbsessions,this.props.sessionsortorder)
     //pushing agent email to array for sending push notifications
 
     agentemail.push(this.props.userdetails.email);
@@ -475,7 +475,7 @@ assignSessionToAgent(e){
 
 
             }
-    this.props.add_socket_fb_message(data,this.props.fbchats,this.props.senderid,this.props.fbsessions)
+    this.props.add_socket_fb_message(data,this.props.fbchats,this.props.senderid,this.props.fbsessions,this.props.sessionsortorder)
     //pushing agent email to array for sending push notifications
 
     agentemail.push(this.refs.agentList.options[this.refs.agentList.selectedIndex].dataset.email);
@@ -632,7 +632,7 @@ resolveSession(e){
 
 
             }
-    this.props.add_socket_fb_message(data,this.props.fbchats,this.props.senderid,this.props.fbsessions)
+    this.props.add_socket_fb_message(data,this.props.fbchats,this.props.senderid,this.props.fbsessions,this.props.sessionsortorder)
 
       const usertoken = auth.getToken();
        // 3. update session status on server
