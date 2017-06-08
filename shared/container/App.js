b07ab320b10065798b187c4c4b89581763614dc0
Zarmeen <zarmeennasim@yahoo.com>
2017-06-08 12:32:58 +0500

    adding warning if socket disconnected on UI

--- shared/container/App.js
+++ shared/container/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     super(props, context);
     this.alertme = this.alertme.bind(this);
     this.getSocketmessage = this.getSocketmessage.bind(this);
+    this.state = {'connected':true};
   }
 
   componentDidMount() {
@@ -17,6 +18,18 @@ class App extends Component {
     this.props.route.socket.on('send:fbcustomer', (data) => {
       notify('facebook customer joined');
     });
+    this.props.route.socket.on('disconnect', () => {
+        console.log('disconnecting');  
+        this.setState({'connected':false});
+       // location.reload();
+      });
+
+     this.props.route.socket.on('connect', () => {
+        console.log('disconnecting');  
+        this.setState({'connected':true});
+       // location.reload();
+      });
+
     this.props.route.socket.on('updateFBsessions', (data) => {
       console.log('updateFBsessions desktop notify');
       console.log(data);
@@ -40,8 +53,22 @@ class App extends Component {
     return (
 
       <div>
-
+        {this.state.connected == false &&
+          <div style={{
+                background: '#F44336',
+                width: '100%',
+                fontSize: 14,
+                color: 'white',
+                textAlign: 'center',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                zIndex: 999,
+              
+          }}> You are disconnected. We will keep trying to reconnect or you can Refresh web page... </div>
+        }
         { this.props.children }
+        
 
       </div>
 
