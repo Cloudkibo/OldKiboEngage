e07195d23dd391fd1e945f4ecf9b84e1cc1cfcd0
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-06-09 16:12:39 +0500

    New Chat UI Improved

--- shared/container/Chat/ChatListItem.js
+++ shared/container/Chat/ChatListItem.js
@@ -122,8 +122,10 @@ var res = c.getHours() + ":" + c.getMinutes() + " " + c.toDateString()
 return res;
 }
   var difference = Math.abs(new Date().getTime() / 1000 - new Date(props.customer.requesttime).getTime() / 1000);
-  if (difference < 60) {
-    difference =  difference + ' seconds ago';
+ if(difference < 1){
+   difference =   '1 second ago';
+ }else if (difference < 60) {
+    difference =  Math.floor(difference) + ' seconds ago';
 } else if (difference < 3600) {
     difference = Math.floor(difference / 60) + ' minutes ago';
 } else if (difference < 3600*24) {
@@ -143,21 +145,33 @@ return res;
          {
           (props.new_message_arrived_rid && thisChat == props.customer.request_id  ?
 
-           <div className="list-group-item" style={{...changecc, background: 'black'}}>
+           <div className="list-group-item uk-card-hover uk-animation-toggle" style={{...changecc, border: 0, background: '#1CAF9A', color: 'white'}}>
 
-            <h4 className = 'list-group-item-heading' style={hleft}>{props.customer.customerid.name?props.customer.customerid.name : props.customer.customerid.customerID}</h4>
+            <p className = 'list-group-item-heading' style={{...hleft, fontSize: 15, color: 'white'}}><img src="img/user.png" className="uk-border-rounded" style={{maxWidth: 35, maxHeight:35}}/> {props.customer.customerid.name?props.customer.customerid.name : props.customer.customerid.customerID}</p>
+             <div className="uk-align-right">
+            <p className="uk-text-meta " style={{color: 'white'}} ><span className='badge' style={rightStyle}>{unreadCount}</span> {difference} </p>
+            </div>
+            
+                    <div className="uk-animation-fast uk-animation-shake">
+                        <span className="uk-badge uk-animation-shake" style={{fontSize: 10, margin:5}}>{props.customer.status}</span>
+                        
+                    </div>
+            
 
-             <span className='badge' style={rightStyle}></span>:<span className='badge' style={rightStyle}>{unreadCount}</span>
-
+           
             <div style={divMargin}>
+            
+              {/*
             <span style={leftStyle}>{ag[0].deptname}</span>
             <span  style={rightAgent}><i className="glyphicon glyphicon-time"/>{handleDate(props.customer.requesttime)}</span>
             <br/>
             <span  style={rightAgent}><i className="fa fa-headphones"/>{ch[0].msg_channel_name}</span>
             <br/>
             <span  style={rightAgent}><i className="fa fa-headphones"/>{props.customer.status}</span>
+         */}
            <br/>
            {
+            
             (agentname.length > 0?
             <span  style={rightAgent}><i className="fa fa-user"/>{agentname[0]}</span>:<span/>
            )}
